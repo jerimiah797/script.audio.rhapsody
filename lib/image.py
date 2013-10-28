6faf7ad322cd9c857fb5690f7d67a5de8e7fc4a8
Jerimiah Ham <jerimiah797@gmail.com>
2013-10-28 14:52:49 -0700

    added toptracks and topartists to rhapapi

---
+++ lib/image.py
@@ -0,0 +1,9 @@
+import urllib
+import os
+
+class Images():
+
+	def __init__(self):
+		pass
+
+	
--- lib/rhapapi.py
+++ lib/rhapapi.py
@@ -16,22 +16,6 @@ class Api():
 		self.APIKEY = "22Q1bFiwGxYA2eaG4vVAGsJqi3SQWzmd"
 		self.SECRET = "Z1AAYBC1JEtnMJGm"
 
-	def __build_member_req(self, url, access_token):
-		header = b'Bearer ' + access_token
-		req = urllib2.Request(url)
-		req.add_header('Authorization', header)
-		return req
-
-	def login_member(self, username, password):
-		data = urllib.urlencode({'username': username, 'password': password, 'grant_type': 'password'})
-		header = b'Basic ' + base64.b64encode(self.APIKEY + b':' + self.SECRET)
-		req = urllib2.Request(self.AUTHURL, data)
-		req.add_header('Authorization', header)
-		results = self.__get_data_from_rhapsody(req)
-		if results:
-			return results
-		else:
-			return False
 
 	def __get_data_from_rhapsody(self, req):
 		succeed = 0
@@ -49,6 +33,27 @@ class Api():
 		return False
 
 
+#----------- Secure API calls requiring auth headers ---------
+
+	def __build_member_req(self, url, access_token):
+		header = b'Bearer ' + access_token
+		req = urllib2.Request(url)
+		req.add_header('Authorization', header)
+		return req
+
+
+	def login_member(self, username, password):
+		data = urllib.urlencode({'username': username, 'password': password, 'grant_type': 'password'})
+		header = b'Basic ' + base64.b64encode(self.APIKEY + b':' + self.SECRET)
+		req = urllib2.Request(self.AUTHURL, data)
+		req.add_header('Authorization', header)
+		results = self.__get_data_from_rhapsody(req)
+		if results:
+			return results
+		else:
+			return False
+
+
 	def get_playable_url(self, track_id, access_token):
 		url = "%splay/%s" %(self.S_BASEURL, track_id)
 		req = self.__build_member_req(url, access_token)
@@ -58,6 +63,20 @@ class Api():
 		else:
 			return False
 
+	def validate_session(self):
+		pass
+
+	def get_session(self):
+		pass
+
+	def log_playstart(self):
+		pass
+
+	def log_playstop(self):
+		pass
+
+
+#----------- Normal API calls  ---------
 
 	def get_album_review(self, album_id):
 		url = "%salbums/%s/reviews?apikey=%s" % (self.BASEURL, album_id, self.APIKEY)
@@ -96,6 +115,22 @@ class Api():
 
 	def get_top_albums(self):
 		url = '%salbums/top?apikey=%s&limit=100' % (self.BASEURL, self.APIKEY)
+		results = self.__get_data_from_rhapsody(url)
+		if results:
+			return results
+		else:
+			return False
+
+	def get_top_artists(self):
+		url = '%sartists/top?apikey=%s&limit=100' % (self.BASEURL, self.APIKEY)
+		results = self.__get_data_from_rhapsody(url)
+		if results:
+			return results
+		else:
+			return False
+
+	def get_top_tracks(self):
+		url = '%stracks/top?apikey=%s&limit=100' % (self.BASEURL, self.APIKEY)
 		results = self.__get_data_from_rhapsody(url)
 		if results:
 			return results
