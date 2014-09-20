c197121315508d1328c82ccdbd075757d3c0075d
Jerimiah Ham <jerimiah797@gmail.com>
2014-09-20 09:26:51 -0700

    Revert "started working on offline messaging"
    This reverts commit 1f4dfc8273e9ad7b65e197931b6087f8ebbd48fe.

--- lib/view.py
+++ lib/view.py
@@ -284,8 +284,6 @@ class MainWin(WinBase):
 		self.handle = xbmcgui.Window(xbmcgui.getCurrentWindowId())
 		self.handle.setProperty("browseview", self.app.view_keeper['browseview'])
 		self.handle.setProperty("frame", self.app.view_keeper['frame'])
-		if OFFLINE == True:
-			self.handle.setProperty("offline", "true")
 		self.main()
 
 	def main(self):
