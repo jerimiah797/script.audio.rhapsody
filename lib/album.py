42fb0bea1b1f5ef5f518e78e56411d75acaa11b8
Julian Scholle <Julian.Scholle@googlemail.com>
2014-12-28 16:11:34 +0100

    add item classes

---
+++ lib/album.py
@@ -0,0 +1,11 @@
+class Album():
+    id = 0
+    name = ""
+    artist = 0
+    tags = list()
+    images = list()
+    tracks = list()
+
+    def __init__(self):
+        pass
+
---
+++ lib/track.py
@@ -0,0 +1,10 @@
+class Track():
+    id = 0
+    name = ""
+    album = 0
+    genre = 0
+    sample = ""
+    duration = 0
+
+    def __init__(self):
+        pass
